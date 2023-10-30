b1c801b63c4fe4cbe0eca86530699903435eb0e0
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-30 16:07:33 +0500

    feat: data access interfaces add

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Artist> Artists { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken =default);
+}
---
+++ Domain/Abstractions/IArtistRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions;
+
+public interface IArtistRepository
+{
+    Task<Artist?> GetArtistByIdAsync(Guid id);
+
+    Task<Artist?> GetArtistByNameAsync(string name);
+    void Add(Artist artist);
+    void Remove(Artist artist);
+}
