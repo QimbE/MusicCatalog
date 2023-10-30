b62d750a3c0a927b15f218f973bd7e50687e60ca
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-30 16:09:07 +0500

    feat: base exceptions add

---
+++ Domain/Exceptions/ArtistNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions;
+
+public sealed class ArtistNotFoundException : Exception
+{
+    public ArtistNotFoundException(Guid id)
+        :base($"The product with the Id = {id} was not found.")
+    {
+        
+    }
+}
---
+++ Domain/Exceptions/ArtistWithTheSameNameException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Domain.Exceptions;
+
+public class ArtistWithTheSameNameException : DuplicateNameException
+{
+    public ArtistWithTheSameNameException(string name)
+        :base($"Artist with the name {name} already exists.")
+    {
+        
+    }
+}
