63ddc5c0dd5e8b0dc0bd2ca9f1e6dd1d03e8c0ca
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-31 01:30:01 +0500

    feat: add result type

---
+++ Application/Common/Result.cs
@@ -0,0 +1,272 @@
+﻿namespace Application.Common;
+
+/// <summary>
+/// Slightly modified ErrorOr library by Amichai Mantinband
+/// </summary>
+/// <typeparam name="TValue">Some excepcted value</typeparam>
+public record struct Result<TValue>
+{
+    private readonly TValue? _value = default;
+    private readonly List<Exception>? _errors = null;
+
+    /// <summary>
+    /// Gets a value indicating whether the state is error.
+    /// </summary>
+    public bool IsError { get; }
+
+    /// <summary>
+    /// Gets the list of errors. If the state is not error, the list will contain a single error representing the state.
+    /// </summary>
+    public List<Exception> Errors => IsError ? _errors! : new List<Exception> { };
+
+    /// <summary>
+    /// Gets the list of errors. If the state is not error, the list will be empty.
+    /// </summary>
+    public List<Exception> ErrorsOrEmptyList => IsError ? _errors! : new();
+
+    /// <summary>
+    /// Creates an <see cref="Result{TValue}"/> from a list of errors.
+    /// </summary>
+    public static Result<TValue> From(List<Exception> errors)
+    {
+        return errors;
+    }
+
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    public TValue Value => _value!;
+
+    /// <summary>
+    /// Gets the first error.
+    /// </summary>
+    public Exception? FirstError
+    {
+        get
+        {
+            if (!IsError)
+            {
+                return null;
+            }
+
+            return _errors![0];
+        }
+    }
+
+    private Result(Exception error)
+    {
+        _errors = new List<Exception> { error };
+        IsError = true;
+    }
+
+    private Result(List<Exception> errors)
+    {
+        _errors = errors;
+        IsError = true;
+    }
+
+    private Result(TValue value)
+    {
+        _value = value;
+        IsError = false;
+    }
+
+    /// <summary>
+    /// Creates an <see cref="Result{TValue}"/> from a value.
+    /// </summary>
+    public static implicit operator Result<TValue>(TValue value)
+    {
+        return new Result<TValue>(value);
+    }
+
+    /// <summary>
+    /// Creates an <see cref="Result{TValue}"/> from an error.
+    /// </summary>
+    public static implicit operator Result<TValue>(Exception error)
+    {
+        return new Result<TValue>(error);
+    }
+
+    /// <summary>
+    /// Creates an <see cref="Result{TValue}"/> from a list of errors.
+    /// </summary>
+    public static implicit operator Result<TValue>(List<Exception> errors)
+    {
+        return new Result<TValue>(errors);
+    }
+
+    /// <summary>
+    /// Creates an <see cref="Result{TValue}"/> from a list of errors.
+    /// </summary>
+    public static implicit operator Result<TValue>(Exception[] errors)
+    {
+        return new Result<TValue>(errors.ToList());
+    }
+
+    /// <summary>
+    /// Executes the appropriate action based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is an error, the provided action <paramref name="onError"/> is executed.
+    /// If the state is a value, the provided action <paramref name="onValue"/> is executed.
+    /// </summary>
+    /// <param name="onValue">The action to execute if the state is a value.</param>
+    /// <param name="onError">The action to execute if the state is an error.</param>
+    public void Switch(Action<TValue> onValue, Action<List<Exception>> onError)
+    {
+        if (IsError)
+        {
+            onError(Errors);
+            return;
+        }
+
+        onValue(Value);
+    }
+
+    /// <summary>
+    /// Asynchronously executes the appropriate action based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is an error, the provided action <paramref name="onError"/> is executed asynchronously.
+    /// If the state is a value, the provided action <paramref name="onValue"/> is executed asynchronously.
+    /// </summary>
+    /// <param name="onValue">The asynchronous action to execute if the state is a value.</param>
+    /// <param name="onError">The asynchronous action to execute if the state is an error.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task SwitchAsync(Func<TValue, Task> onValue, Func<List<Exception>, Task> onError)
+    {
+        if (IsError)
+        {
+            await onError(Errors).ConfigureAwait(false);
+            return;
+        }
+
+        await onValue(Value).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Executes the appropriate action based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is an error, the provided action <paramref name="onFirstError"/> is executed using the first error as input.
+    /// If the state is a value, the provided action <paramref name="onValue"/> is executed.
+    /// </summary>
+    /// <param name="onValue">The action to execute if the state is a value.</param>
+    /// <param name="onFirstError">The action to execute with the first error if the state is an error.</param>
+    public void SwitchFirst(Action<TValue> onValue, Action<Exception> onFirstError)
+    {
+        if (IsError)
+        {
+            onFirstError(FirstError);
+            return;
+        }
+
+        onValue(Value);
+    }
+
+    /// <summary>
+    /// Asynchronously executes the appropriate action based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is an error, the provided action <paramref name="onFirstError"/> is executed asynchronously using the first error as input.
+    /// If the state is a value, the provided action <paramref name="onValue"/> is executed asynchronously.
+    /// </summary>
+    /// <param name="onValue">The asynchronous action to execute if the state is a value.</param>
+    /// <param name="onFirstError">The asynchronous action to execute with the first error if the state is an error.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task SwitchFirstAsync(Func<TValue, Task> onValue, Func<Exception, Task> onFirstError)
+    {
+        if (IsError)
+        {
+            await onFirstError(FirstError).ConfigureAwait(false);
+            return;
+        }
+
+        await onValue(Value).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Executes the appropriate function based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed and its result is returned.
+    /// If the state is an error, the provided function <paramref name="onError"/> is executed and its result is returned.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="onValue">The function to execute if the state is a value.</param>
+    /// <param name="onError">The function to execute if the state is an error.</param>
+    /// <returns>The result of the executed function.</returns>
+    public TResult Match<TResult>(Func<TValue, TResult> onValue, Func<List<Exception>, TResult> onError)
+    {
+        if (IsError)
+        {
+            return onError(Errors);
+        }
+
+        return onValue(Value);
+    }
+
+    /// <summary>
+    /// Asynchronously executes the appropriate function based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed asynchronously and its result is returned.
+    /// If the state is an error, the provided function <paramref name="onError"/> is executed asynchronously and its result is returned.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="onValue">The asynchronous function to execute if the state is a value.</param>
+    /// <param name="onError">The asynchronous function to execute if the state is an error.</param>
+    /// <returns>A task representing the asynchronous operation that yields the result of the executed function.</returns>
+    public async Task<TResult> MatchAsync<TResult>(Func<TValue, Task<TResult>> onValue, Func<List<Exception>, Task<TResult>> onError)
+    {
+        if (IsError)
+        {
+            return await onError(Errors).ConfigureAwait(false);
+        }
+
+        return await onValue(Value).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Executes the appropriate function based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed and its result is returned.
+    /// If the state is an error, the provided function <paramref name="onFirstError"/> is executed using the first error, and its result is returned.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="onValue">The function to execute if the state is a value.</param>
+    /// <param name="onFirstError">The function to execute with the first error if the state is an error.</param>
+    /// <returns>The result of the executed function.</returns>
+    public TResult MatchFirst<TResult>(Func<TValue, TResult> onValue, Func<Exception, TResult> onFirstError)
+    {
+        if (IsError)
+        {
+            return onFirstError(FirstError);
+        }
+
+        return onValue(Value);
+    }
+
+    /// <summary>
+    /// Asynchronously executes the appropriate function based on the state of the <see cref="Result{TValue}"/>.
+    /// If the state is a value, the provided function <paramref name="onValue"/> is executed asynchronously and its result is returned.
+    /// If the state is an error, the provided function <paramref name="onFirstError"/> is executed asynchronously using the first error, and its result is returned.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="onValue">The asynchronous function to execute if the state is a value.</param>
+    /// <param name="onFirstError">The asynchronous function to execute with the first error if the state is an error.</param>
+    /// <returns>A task representing the asynchronous operation that yields the result of the executed function.</returns>
+    public async Task<TResult> MatchFirstAsync<TResult>(Func<TValue, Task<TResult>> onValue, Func<Exception, Task<TResult>> onFirstError)
+    {
+        if (IsError)
+        {
+            return await onFirstError(FirstError).ConfigureAwait(false);
+        }
+
+        return await onValue(Value).ConfigureAwait(false);
+    }
+}
+
+/// <summary>
+/// Provides utility methods for creating instances of <see ref="Result{T}"/>.
+/// </summary>
+public static class Result
+{
+    /// <summary>
+    /// Creates an <see ref="Result{TValue}"/> instance from a value.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <param name="value">The value from which to create an Result instance.</param>
+    /// <returns>An <see ref="Result{TValue}"/> instance containing the specified value.</returns>
+    public static Result<TValue> From<TValue>(TValue value)
+    {
+        return value;
+    }
+}
