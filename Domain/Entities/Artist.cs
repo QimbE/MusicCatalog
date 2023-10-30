b05f3bdaa2fcae643bc2e4faec326e16896d0798
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-30 16:06:43 +0500

    feat: base entity add

---
+++ Domain/Entities/Artist.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public sealed class Artist : Entity
+{
+    public string Name { get; private set; }
+
+    public string? Description { get; private set; }
+    
+    private Artist()
+    {
+        
+    }
+    
+    public Artist(Guid id, string name, string? description = null)
+        : base(id)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public void Update(string name, string? description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
---
+++ Domain/Primitives/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Primitives;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+    
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    protected Entity()
+    {
+        
+    }
+}
