3346d2d2f8c868bf33eaec2053fef7648eb94b7a
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-31 18:27:52 +0500

    feat: add logging behavior

---
+++ Application/Behaviors/LoggingPipeLineBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Behaviors;
+
+public class LoggingPipeLineBehavior<TRequest, TResult>: IPipelineBehavior<TRequest, Result<TResult>>
+    where TRequest: IRequest<Result<TResult>>
+{
+    private readonly ILogger<LoggingPipeLineBehavior<TRequest, TResult>> _logger;
+
+    public LoggingPipeLineBehavior(ILogger<LoggingPipeLineBehavior<TRequest, TResult>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<Result<TResult>> Handle(
+        TRequest request,
+        RequestHandlerDelegate<Result<TResult>> next,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Starting request {@RequestName}, {@DateTimeUtc}",
+            typeof(TRequest).Name,
+            DateTime.UtcNow);
+        
+        var result = await next();
+
+        if (result.IsError)
+        {
+            _logger.LogError("Requst failure {@RequestName}, {@Error} {@DateTimeUtc}",
+                typeof(TRequest).Name,
+                result.FirstError,
+                DateTime.UtcNow);
+        }
+        
+        _logger.LogInformation("Completed request {@RequestName}, {@DateTimeUtc}",
+            typeof(TRequest).Name,
+            DateTime.UtcNow);
+        
+        return result;
+    }
+}
