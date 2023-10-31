ef5745ba700e754687a6fe4480d8273dc4bfa4d9
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-31 18:41:31 +0500

    feat: add validators

---
+++ Application/Artists/Create/CreateArtistCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Artists.Create;
+
+public class CreateArtistCommandValidator: AbstractValidator<CreateArtistCommand>
+{
+    public CreateArtistCommandValidator()
+    {
+        RuleFor(a => a.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+        
+        RuleFor(x => x.Description)
+            .MaximumLength(2000);
+    }
+}
---
+++ Application/Artists/Update/UpdateArtistCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Artists.Update;
+
+public class UpdateArtistCommandValidator: AbstractValidator<UpdateArtistCommand>
+{
+    public UpdateArtistCommandValidator()
+    {
+        RuleFor(a => a.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+        
+        RuleFor(x => x.Description)
+            .MaximumLength(2000);
+    }
+}
