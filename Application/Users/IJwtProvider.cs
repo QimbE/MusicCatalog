7d65a64e70e7808899a0fdfe6716516b9554636a
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-19 16:54:04 +0500

    feat: add jwt token provider

---
+++ Application/Users/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Users;
+
+namespace Application.Users;
+
+public interface IJwtProvider
+{
+    string Generate(User user);
+}
---
+++ Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Users;
+using Domain.Users;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Authentication;
+
+public sealed class JwtProvider: IJwtProvider
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public string Generate(User user)
+    {
+        var claims = new Claim[]
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email)
+        };
+        
+        var settings = _configuration.GetSection("JwtSettings");
+        
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(settings["Key"])),
+            SecurityAlgorithms.HmacSha256);
+        
+        var token = new JwtSecurityToken(
+            settings["Issuer"],
+            settings["Audience"],
+            claims,
+            null,
+            DateTime.UtcNow.AddHours(2),
+            signingCredentials
+            );
+
+        string tokenValue = new JwtSecurityTokenHandler()
+            .WriteToken(token);
+
+        return tokenValue;
+    }
+}
