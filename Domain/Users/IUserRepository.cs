9c488f6fa5ae62e387481e0154d3aaac3b9c44dd
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-19 21:18:28 +0500

    feat: add operations to user repository

--- Domain/Users/IUserRepository.cs
+++ Domain/Users/IUserRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace Domain.Users;
+﻿using System.Linq.Expressions;
+
+namespace Domain.Users;
 
 //todo: cancelletion tokens
 public interface IUserRepository
 {
     Task<User?> GetByEmailAsync(string email);
-    
+
+    void Add(User user);
+    Task<bool> Any(Expression<Func<User, bool>> expression);
 }
--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Users;
+﻿using System.Linq.Expressions;
+using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
@@ -15,5 +16,15 @@ public class UserRepository: IUserRepository
     public Task<User?> GetByEmailAsync(string email)
     {
         return _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public void Add(User user)
+    {
+        _context.Users.Add(user);
+    }
+
+    public Task<bool> Any(Expression<Func<User, bool>> expression)
+    {
+        return _context.Users.AnyAsync(expression);
     }
 }
