f8f0c00ab8e9b2c03b5bfe55799e75d734488477
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-19 16:50:21 +0500

    feat: user entity add

---
+++ Domain/Users/User.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+using Domain.Primitives;
+
+namespace Domain.Users;
+
+public class User: Entity
+{
+    public string Username { get; private set; }
+    
+    public string Email { get; private set; }
+    
+    [JsonIgnore]
+    public string Password { get; private set; }
+
+    public UserRole Role { get; private set; }
+
+    private User(string username, string email, string password, UserRole role = UserRole.DefaultUser)
+        :base(Guid.NewGuid())
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+
+    public void Update(string username, string email, string password, UserRole role)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+
+    public static User Create(string username, string email, string password, UserRole role = UserRole.DefaultUser)
+    {
+        return new User(username, email, password, role);
+    }
+}
---
+++ Domain/Users/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Users;
+
+// Fuck Enum Mapping in ef core, todo: intermediate entity mapping
+public enum UserRole
+{
+    DefaultUser = 0,
+    DataBaseAdmin = 1,
+    Admin = 2
+}
