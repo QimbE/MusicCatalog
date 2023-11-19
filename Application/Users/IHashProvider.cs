cfdb28cf35a24d6df5ae8b6a9d8faa05d6ff785f
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-19 21:20:43 +0500

    feat: add hashing class

---
+++ Application/Users/IHashProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Users;
+
+public interface IHashProvider
+{
+    string Hash(string password);
+
+    bool Verify(string passwordHash, string inputPassword);
+}
---
+++ Infrastructure/Authentication/HashProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using Application.Users;
+
+namespace Infrastructure.Authentication;
+
+public class HashProvider: IHashProvider
+{
+
+    private const int SaltSize = 16;
+    private const int KeySize = 32;
+    private const int Iterations = 10000;
+    private static readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA256;
+    private const char Delimiter = ';';
+    
+    public string Hash(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+        var hash = Hash(password, salt);
+
+        return string.Join(Delimiter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+    }
+
+    public bool Verify(string passwordHash, string inputPassword)
+    {
+        var elements = passwordHash.Split(Delimiter);
+        var salt = Convert.FromBase64String(elements[0]);
+        var hash = Convert.FromBase64String(elements[1]);
+
+        var hashInput = Hash(inputPassword, salt);
+
+        return CryptographicOperations.FixedTimeEquals(hash, hashInput);
+    }
+
+    private byte[] Hash(string password, byte[] salt)
+    {
+        return Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithmName, KeySize);
+    }
+}
