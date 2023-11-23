17a6f5504e0ab20289cfb6574305198ee71198c0
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-23 21:25:45 +0500

    feat: add artist graphql query

---
+++ Application/Artists/ArtistsQL.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Application.Data;
+using Domain.Artists;
+using HotChocolate.Authorization;
+using HotChocolate.Types.Descriptors;
+
+namespace Application.Artists;
+
+public class ArtistsQL
+{
+    [UsePaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    // [Authorize(Role.RoleNames.Default)]
+    public async Task<IQueryable<Artist>> GetArtists([Service(ServiceKind.Resolver)] IApplicationDbContext context)
+    {
+        return context.Artists;
+    }
+}
+
