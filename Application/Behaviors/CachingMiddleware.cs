7df4165c377e2f3b91e1c8fa9694d111c32b4d0c
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-23 21:26:50 +0500

    feat: add distributed caching middleware for hot chocolate

---
+++ Application/Behaviors/CachingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Application.Data;
+using HotChocolate.Execution;
+
+namespace Application.Behaviors;
+
+/// <summary>
+/// Hot chocolate redis caching middleware
+/// </summary>
+internal sealed class CachingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CachingMiddleware(
+        RequestDelegate next)
+    {
+        _next = next ??
+                throw new ArgumentNullException(nameof(next));
+    }
+
+    public async ValueTask InvokeAsync(IRequestContext context, [Service] ICacheService cache)
+    {
+        var key = context.Document.ToString();
+        var res = await cache.GetDataAsync<Dictionary<string, object?>?>(key);
+        
+        var isCached = res is not null;
+        
+        if (isCached)
+        {
+            context.Result = new QueryResult(res);
+        }
+        
+        await _next(context).ConfigureAwait(false);
+
+        if (!isCached)
+        {
+            var toCache = ((QueryResult)context.Result).Data;
+            await cache.SetDataAsync(key, toCache, DateTimeOffset.UtcNow.AddSeconds(30));
+        }
+    }
+}
