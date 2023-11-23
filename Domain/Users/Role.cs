4ea6c9ec76efeb9089105eff397f15bf03981df1
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-23 21:22:43 +0500

    feat: add role name constants

--- Domain/Users/Role.cs
+++ Domain/Users/Role.cs
@@ -7,6 +7,13 @@ namespace Domain.Users;
 
 public class Role: SmartEnum<Role>
 {
+    public static class RoleNames
+    {
+        public const string Default = nameof(Default);
+        public const string DatabaseAdmin = nameof(DatabaseAdmin);
+        public const string Admin = nameof(Admin);
+    }
+    
     public static readonly Role Default = new Role(1, nameof(Default));
     public static readonly Role DatabaseAdmin = new Role(2, nameof(DatabaseAdmin));
     public static readonly Role Admin = new Role(3, nameof(Admin));
