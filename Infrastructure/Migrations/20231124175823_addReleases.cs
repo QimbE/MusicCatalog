942653b507db61454dd2fb21bb4636b7effb48a8
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-24 23:45:24 +0500

    feat: add db migration

---
+++ Infrastructure/Migrations/20231124175823_addReleases.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addReleases : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReleaseTypes",
+                columns: table => new
+                {
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReleaseTypes", x => x.Value);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Releases",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TypeId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    ReleaseDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    LinkToCover = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Releases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Releases_Artists_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Releases_ReleaseTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "ReleaseTypes",
+                        principalColumn: "Value",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ReleaseTypes",
+                columns: new[] { "Value", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Album" },
+                    { 2, "Single" },
+                    { 3, "Mixtape" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Releases_AuthorId",
+                table: "Releases",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Releases_Name",
+                table: "Releases",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Releases_TypeId",
+                table: "Releases",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReleaseTypes_Name",
+                table: "ReleaseTypes",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Releases");
+
+            migrationBuilder.DropTable(
+                name: "ReleaseTypes");
+        }
+    }
+}
