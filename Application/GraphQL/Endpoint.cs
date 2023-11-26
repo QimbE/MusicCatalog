c8c3f10e1ec95cc80f16c01e8b08961c45fe205a
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-27 00:10:32 +0500

    feat: add new graphql queries

--- Application/GraphQL/Endpoint.cs
+++ Application/GraphQL/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Data;
 using Domain.Artists;
 using Domain.Releases;
+using Domain.Songs;
 
 namespace Application.GraphQL;
 
@@ -24,6 +25,26 @@ public class Endpoint
     public async Task<IQueryable<Release>> GetReleases([Service(ServiceKind.Resolver)] IApplicationDbContext context, CancellationToken cancellationToken)
     {
         return context.Releases;
+    }
+    
+    [UsePaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    // [Authorize(Role.RoleNames.Default)]
+    public async Task<IQueryable<Song>> GetSongs([Service(ServiceKind.Resolver)] IApplicationDbContext context, CancellationToken cancellationToken)
+    {
+        return context.Songs;
+    }
+    
+    [UsePaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    // [Authorize(Role.RoleNames.Default)]
+    public async Task<IQueryable<Genre>> GetGenres([Service(ServiceKind.Resolver)] IApplicationDbContext context, CancellationToken cancellationToken)
+    {
+        return context.Genres;
     }
 }
 
