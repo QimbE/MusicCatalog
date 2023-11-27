5e66d7facb472387f2606e4aabdf61b69019cf95
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-27 15:34:49 +0500

    feat: add get and post endpoints

---
+++ Presentation/Endpoints/Release.cs
@@ -0,0 +1,39 @@
+﻿using Application.ExceptionHandling.ExpectedErrors;
+using Application.Releases.Create;
+using Application.Releases.Get;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.Endpoints;
+
+public class Release : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("Releases");
+
+        group.MapGet("{id:guid}", async ([FromRoute] Guid id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new GetReleaseQuery(id), cancellationToken);
+
+            return await result.MatchAsync<IResult>(
+                res => Task.FromResult(Results.Ok(new {Message = "Success", Data = res})),
+                errors => sender.Send(new HandleErrorQuery(errors))
+            );
+        });
+
+        group.MapPost("", async ([FromBody] CreateReleaseCommand request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(request, cancellationToken);
+
+            return await result.MatchAsync<IResult>(
+                res => Task.FromResult(Results.Ok(new {Message = "Success", Data = res})),
+                errors => sender.Send(new HandleErrorQuery(errors))
+                );
+        });
+    }
+}
