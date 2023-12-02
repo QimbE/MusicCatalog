fb44454614da52073bf5dac7c8b6140c39f9f0e0
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-02 16:54:08 +0500

    feat: add ExplicitJunctionTable migration

---
+++ Domain/Junction/SongArtist.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Artists;
+using Domain.Songs;
+
+namespace Domain.Junction;
+
+public class SongArtist
+{
+    public Guid SongId { get; set; }
+    
+    public Song Song { get; private set; }
+    
+    public Guid ArtistId { get; set; }
+    
+    public Artist Artist { get; private set; }
+
+    public SongArtist(Guid songId, Guid artistId)
+    {
+        SongId = songId;
+        ArtistId = artistId;
+    }
+}
---
+++ Infrastructure/Migrations/20231202094906_addExplicitJunctionTableConfiguration.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addExplicitJunctionTableConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistSong");
+
+            migrationBuilder.CreateTable(
+                name: "SongArtist",
+                columns: table => new
+                {
+                    SongId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ArtistId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongArtist", x => new { x.ArtistId, x.SongId });
+                    table.ForeignKey(
+                        name: "FK_SongArtist_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SongArtist_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SongArtist_SongId",
+                table: "SongArtist",
+                column: "SongId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongArtist");
+
+            migrationBuilder.CreateTable(
+                name: "ArtistSong",
+                columns: table => new
+                {
+                    ArtistsOnFeatId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SongsOnFeatId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistSong", x => new { x.ArtistsOnFeatId, x.SongsOnFeatId });
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Artists_ArtistsOnFeatId",
+                        column: x => x.ArtistsOnFeatId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Songs_SongsOnFeatId",
+                        column: x => x.SongsOnFeatId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistSong_SongsOnFeatId",
+                table: "ArtistSong",
+                column: "SongsOnFeatId");
+        }
+    }
+}
