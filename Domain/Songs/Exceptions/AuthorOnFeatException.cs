cfcc396f8bc9874dd525dd192ec9234435286a0a
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-02 16:56:46 +0500

    feat: add exceptions for song

---
+++ Domain/Songs/Exceptions/AuthorOnFeatException.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Domain.Songs.Exceptions;
+
+public class AuthorOnFeatException: InvalidConstraintException
+{
+    public AuthorOnFeatException():base("You are trying to add author of song as coauthor")
+    {
+        
+    }
+
+
+}
---
+++ Domain/Songs/Exceptions/GenreNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.Songs.Exceptions;
+
+public class GenreNotFoundException: NotFoundException
+{
+    private const string EntityName = "Genre";
+    public GenreNotFoundException(string propertyName)
+        :base(EntityName, propertyName)
+    {
+        
+    }
+    
+    public GenreNotFoundException(string propertyName, Exception inner)
+        :base(EntityName, propertyName, inner)
+    {
+        
+    }
+}
