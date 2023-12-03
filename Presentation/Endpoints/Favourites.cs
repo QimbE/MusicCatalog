d1c8c10b6e4c514143b487b5df6aaf098a195913
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-04 01:27:02 +0500

    feat: map favourites endpoints

---
+++ Presentation/Endpoints/Favourites.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Claims;
+using Application.Authorization;
+using Application.ExceptionHandling.ExpectedErrors;
+using Application.Songs.AddToFavourites;
+using Application.Songs.RemoveFromFavourites;
+using Carter;
+using Domain.Users;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.Endpoints;
+
+public class Favourites:ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("Favourites").WithTags("Favourites").UserShouldBeAtLeast(Role.Default);
+
+        group.MapPost("", async (HttpRequest request, [FromBody] Guid songId, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var userIdClaim = request.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            if (userIdClaim is null)
+            {
+                return Results.Unauthorized();
+            }
+
+            var command = new AddToFavouritesCommand(Guid.Parse(userIdClaim.Value), songId);
+            
+            var result = await sender.Send(command, cancellationToken);
+            
+            return await result.MatchAsync<IResult>(
+                res => Task.FromResult(Results.Ok(new {Message = "Success", Data = res})),
+                errors => sender.Send(new HandleErrorQuery(errors))
+            );
+        });
+
+        group.MapDelete("", async (HttpRequest request, [FromBody] Guid songId, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var userIdClaim = request.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            if (userIdClaim is null)
+            {
+                return Results.Unauthorized();
+            }
+
+            var command = new RemoveFromFavouritesCommand(Guid.Parse(userIdClaim.Value), songId);
+            
+            var result = await sender.Send(command, cancellationToken);
+            
+            return await result.MatchAsync<IResult>(
+                res => Task.FromResult(Results.Ok(new {Message = "Success", Data = res})),
+                errors => sender.Send(new HandleErrorQuery(errors))
+            );
+        });
+    }
+}
