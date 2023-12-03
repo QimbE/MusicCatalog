76b44067e232b906732558daaf89e935b9be0d52
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-04 01:26:31 +0500

    feat: add remove from favourites command

---
+++ Application/Songs/RemoveFromFavourites/RemoveFromFavouritesCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Common;
+using MediatR;
+
+namespace Application.Songs.RemoveFromFavourites;
+
+public record RemoveFromFavouritesCommand(Guid UserId, Guid SongId):IRequest<ResultType<bool>>;
---
+++ Application/Songs/RemoveFromFavourites/RemoveFromFavouritesCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common;
+using Application.Data;
+using Domain.Songs.Exceptions;
+using Domain.Users.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Songs.RemoveFromFavourites;
+
+public class RemoveFromFavouritesCommandHandler:IRequestHandler<RemoveFromFavouritesCommand, ResultType<bool>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public RemoveFromFavouritesCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ResultType<bool>> Handle(RemoveFromFavouritesCommand request, CancellationToken cancellationToken)
+    {
+        var favourite = await _context.SongUsers
+            .SingleOrDefaultAsync(su =>
+                su.SongId == request.SongId && su.UserId == request.UserId, cancellationToken
+                );
+        
+        if (favourite is null)
+        {
+            return new SongIsNotInFavouritesException(request.SongId);
+        }
+
+        _context.SongUsers.Remove(favourite);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
