824548bc97ec09c7d78ff42e68670190ba8f333c
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-04 01:25:46 +0500

    feat: add songuser exceptions

---
+++ Domain/Songs/Exceptions/SongIsAlreadyInFavouritesException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Domain.Songs.Exceptions;
+
+public class SongIsAlreadyInFavouritesException : DuplicateNameException
+{
+    public SongIsAlreadyInFavouritesException(Guid songId)
+        :base($"Song with Id {songId} is already in favourites.")
+    {
+        
+    }
+}
---
+++ Domain/Songs/Exceptions/SongIsNotInFavouritesException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Domain.Songs.Exceptions;
+
+public class SongIsNotInFavouritesException: DuplicateNameException
+{
+    public SongIsNotInFavouritesException(Guid songId)
+        :base($"Song with Id {songId} is not in favourites.")
+    {
+        
+    }
+}
