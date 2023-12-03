eee7fd9acd8ae61e6583ede55cb4a85dc31b7c56
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-04 01:24:14 +0500

    feat: add migrations

---
+++ Infrastructure/Migrations/20231203155049_addExplicitFavouritesTableConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addExplicitFavouritesTableConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Songs_FavouriteSongsId",
+                table: "SongUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Users_UsersWhoAddedId",
+                table: "SongUser");
+
+            migrationBuilder.RenameColumn(
+                name: "UsersWhoAddedId",
+                table: "SongUser",
+                newName: "SongId");
+
+            migrationBuilder.RenameColumn(
+                name: "FavouriteSongsId",
+                table: "SongUser",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SongUser_UsersWhoAddedId",
+                table: "SongUser",
+                newName: "IX_SongUser_SongId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Songs_SongId",
+                table: "SongUser",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Users_UserId",
+                table: "SongUser",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Songs_SongId",
+                table: "SongUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Users_UserId",
+                table: "SongUser");
+
+            migrationBuilder.RenameColumn(
+                name: "SongId",
+                table: "SongUser",
+                newName: "UsersWhoAddedId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "SongUser",
+                newName: "FavouriteSongsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SongUser_SongId",
+                table: "SongUser",
+                newName: "IX_SongUser_UsersWhoAddedId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Songs_FavouriteSongsId",
+                table: "SongUser",
+                column: "FavouriteSongsId",
+                principalTable: "Songs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Users_UsersWhoAddedId",
+                table: "SongUser",
+                column: "UsersWhoAddedId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20231203191606_addExplicitSongUsersTable.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addExplicitSongUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Songs_SongId",
+                table: "SongUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUser_Users_UserId",
+                table: "SongUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SongUser",
+                table: "SongUser");
+
+            migrationBuilder.RenameTable(
+                name: "SongUser",
+                newName: "SongUsers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SongUser_SongId",
+                table: "SongUsers",
+                newName: "IX_SongUsers_SongId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SongUsers",
+                table: "SongUsers",
+                columns: new[] { "UserId", "SongId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUsers_Songs_SongId",
+                table: "SongUsers",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUsers_Users_UserId",
+                table: "SongUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUsers_Songs_SongId",
+                table: "SongUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SongUsers_Users_UserId",
+                table: "SongUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SongUsers",
+                table: "SongUsers");
+
+            migrationBuilder.RenameTable(
+                name: "SongUsers",
+                newName: "SongUser");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SongUsers_SongId",
+                table: "SongUser",
+                newName: "IX_SongUser_SongId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SongUser",
+                table: "SongUser",
+                columns: new[] { "UserId", "SongId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Songs_SongId",
+                table: "SongUser",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SongUser_Users_UserId",
+                table: "SongUser",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
